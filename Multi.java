3933b34f1a4ae470b4c4a2527247437d59791be7
Yash chaubey <yash11edu@gmail.com>
2024-02-01 08:45:05 +0530

    diff.java

---
+++ Multi.java
@@ -0,0 +1,9 @@
+public class Multi{
+    public static void main(String[] args){
+        int a=5;
+        int b =5;
+        int c = 11;
+        System.out.println(a*b);
+          System.out.println(a*b*c);
+    }
+}
